1380b122f3c0251cc2047d70c598235348d49a33
Akash-akp <akashakp0037@gmail.com>
2024-12-04 20:50:43 +0530

    add Explicit Wait command

--- src/main/java/org/example/TestSelenium.java
+++ src/main/java/org/example/TestSelenium.java
@@ -12,20 +12,35 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import java.time.Duration;
 
 public class TestSelenium {
+
+    public static WebElement waitForElementLocatedBy(WebDriver driver,By path){
+        return new WebDriverWait(driver,Duration.ofSeconds(15)).until(ExpectedConditions.presenceOfElementLocated(path));
+    }
+
     public static void main(String[] args) {
         try {
             // Set up ChromeDriver
 //            System.setProperty("webdriver.chrome.driver", "path/to/chromedriver"); // Replace with actual path
             WebDriver chromeDriver = new ChromeDriver();
             chromeDriver.get("https://www.google.com");
-            new WebDriverWait(chromeDriver,Duration.ofSeconds(15)).until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id='APjFqb']")));
-            By SearchBar = By.xpath("//*[@id='APjFqb']");
-            WebElement search_bar = chromeDriver.findElement(SearchBar);
+
+
+//            new WebDriverWait(chromeDriver,Duration.ofSeconds(1)).until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id='APjFqb']")));
+//            By SearchBar = By.xpath("//*[@id='APjFqb']");
+//            WebElement search_bar = chromeDriver.findElement(SearchBar);
+
+//            WebElement search_bar = new WebDriverWait(chromeDriver,Duration.ofSeconds(1)).until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id='APjFqb']")));
+
+            WebElement search_bar = waitForElementLocatedBy(chromeDriver,By.xpath("//*[@id='APjFqb']"));
+
             search_bar.sendKeys("Akash Kumar Parida");
-            new WebDriverWait(chromeDriver,Duration.ofSeconds(15)).until(ExpectedConditions.presenceOfElementLocated(By.xpath("/html/body/div[1]/div[3]/form/div[1]/div[1]/div[3]/center/input[1]")));
-            WebElement search_button = chromeDriver.findElement(By.xpath("/html/body/div[1]/div[3]/form/div[1]/div[1]/div[3]/center/input[1]"));
+//            new WebDriverWait(chromeDriver,Duration.ofSeconds(15)).until(ExpectedConditions.presenceOfElementLocated(By.xpath("/html/body/div[1]/div[3]/form/div[1]/div[1]/div[3]/center/input[1]")));
+//            WebElement search_button = chromeDriver.findElement(By.xpath("/html/body/div[1]/div[3]/form/div[1]/div[1]/div[3]/center/input[1]"));
+
+            WebElement search_button = waitForElementLocatedBy(chromeDriver,By.xpath("/html/body/div[1]/div[3]/form/div[1]/div[1]/div[3]/center/input[1]"));
+
             search_button.click();
-//            Thread.sleep(2000);
+            Thread.sleep(2000);
 //            chromeDriver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(3000));
 //            new WebDriverWait(chromeDriver,Duration.ofSeconds(3000));
             chromeDriver.quit();
